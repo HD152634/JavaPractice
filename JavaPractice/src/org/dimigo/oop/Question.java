5e2f9568ab93a57479289c5a7e86af1f34e2cffa
hana4 <hana4@DESKTOP-P4D391Q>
2017-04-03 12:52:10 +0900

    실습과제 5 my favorite

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+ * <pre> 
+ * org.dimigo.oop
+ * 	 |_  Question 
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 4. 3.
+ * </pre>
+ *
+ * @author		: hana4
+ * @version		: 1.0
+ */
+public class Question {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String[]questions = {"1.가장 좋아하는 가수는?","2.가장 좋아하는 배우는?","3.가장 좋아하는 과목은?"};
+		String[]answer = {"방탄","조정석","문학"};
+		
+		Scanner scanner = new Scanner(System.in);
+	
+
+		for(int i=0; i<3; i++){
+			System.out.println(questions[i]);
+			String answers = scanner.nextLine();
+			if(answer[i].equals(answers)){
+				System.out.println("정답입니다");
+			}
+			else System.out.println("틀렸습니다");
+		}
+	System.out.println("<<결과출력>>");
+	StringBuilder sb= new StringBuilder();
+	for(int i=0; i<3; i++){
+		sb.append(questions[i]).append(answer[i]).append("입니다.\n");
+	}
+	System.out.println(sb);
+	}
+}
