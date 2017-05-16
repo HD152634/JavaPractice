c994067e397640ffa38c502f7c3d228482298df7
hana4 <hana4@DESKTOP-P4D391Q>
2017-05-16 14:40:46 +0900

    실습과제12

---
+++ JavaPractice/src/org/dimigo/abstractclass/SmartPhone.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package org.dimigo.abstractclass;
+
+/**
+ * <pre> 
+ * org.dimigo.inheritance
+ * 	 |_  SmartPhone 
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 16.
+ * </pre>
+ *
+ * @author		: hana4
+ * @version		: 1.0
+ */
+public abstract class SmartPhone {
+
+	public String model;
+	public String company;
+	public int price;
+	
+	public SmartPhone(){
+		
+	}
+	public SmartPhone(String model, String company,int price){
+		this.model = model;
+		this.company = company;
+		this.price = price;
+	}
+	
+	public void turnOn(){
+		System.out.println(model+"의 전원을 켭니다");
+	}
+	public void turnOff(){
+		System.out.println(model+"의 전원을 끕니다");
+	}
+	public abstract void pay();
+	
+	public void useSpecialFunction(){
+		if (this instanceof IPhone){
+			((IPhone)this).useAirDrop();
+		}
+		else {
+			((Galaxy)this).useWirelessCharging();
+		}
+	}
+	public String toString(){
+		return "모델명:"+model+",제조사:"+company+",가격:"+String.format("%,d",price)+"원";
+	}
+
+}
